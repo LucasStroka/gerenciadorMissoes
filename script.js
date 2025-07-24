9b6d63a5193e000100fc8db4614bfb4e417a5398
Alex <alex.ribas12@gmail.com>
2025-07-24 09:49:36 -0300

    Filtrar Prioridade

--- script.js
+++ script.js
@@ -21,3 +21,40 @@ const arrayMission = []
 
 
 
+function filtrarPrioridade() {
+    if (arrayMission.length === 0) {
+        console.log('Nenhuma missão em andamento!\nPressione ENTER para voltar...');
+        rl.question('', MENU => {});
+        return;
+    }
+
+    const prioridade = [...new Set(arrayMission.map(missao => missao.prioridade))];
+
+    console.log('Níveis de Prioridade:\n');
+    prioridade.forEach((p, index) => {
+        console.log(`${index + 1}. ${p}`);
+    });
+
+    rl.question('Digite qual nível de prioridade deseja verificar:\n', (input) => {
+        const indice = parseInt(input) - 1;
+
+        if (isNaN(indice) || indice < 0 || indice >= prioridade.length) {
+            console.log('Digite um número válido\n');
+            return filtrarPrioridade();
+        }
+
+        const prioridadeEscolhida = prioridade[indice];
+        const nivelPrioridade = arrayMission.filter(missao => missao.prioridade === prioridadeEscolhida);
+
+        console.log(`\nMissões com a prioridade ${prioridadeEscolhida}:`);
+        nivelPrioridade.forEach((missao, index) => {
+            const tripulantes = missao.arrayTripulantes.length > 0
+                ? missao.arrayTripulantes.join(', ')
+                : 'Nave vazia';
+            console.log(`${index + 1}. Missão: ${missao.destino}, Tripulantes: ${tripulantes}, Prioridade: ${missao.prioridade}`);
+        });
+
+        console.log('\nPressione ENTER para voltar...');
+        rl.question('', MENU => {});
+    });
+}
