8e0f1280969faa7e9ed3c587765c49e2617e9cce
VitorInacioBorges <vitorinacioborges@gmail.com>
2025-07-24 09:38:56 -0300

    concluir missao

--- script.js
+++ script.js
@@ -17,7 +17,27 @@ let mission = {
     stats : " "
 }
 
-const arrayMission = []
+const arrayMission = [];
 
-
-
+function concludeMission(){
+    if(arrayMission.length == 0){
+        console.log('Sem missões para concluir.');
+        menu();
+    } else {
+        for(let i = 0; i < arrayMission.length; i++){
+            console.log('========= MISSÕES LISTADAS =========');
+            console.log(`${i + 1} - Nome: ${arrayMission.nomeMissao}, Destino: ${arrayMission.destino}, Prioridade: ${arrayMission.prioridade}, Tripulantes: ${arrayMission.arrayTripulantes}, Status: ${arrayMission.stats}`);
+        }
+        rl.question('Escreva o número da missão que você quer concluir: ', (input) => {
+            const index = parseInt(input) - 1;
+            if(isNaN(index) || index < 0 || index > arrayMission.length){
+                console.log('Invalid option!!');
+                menu();
+            } else {
+                arrayMission[index].stats = 'concluida';
+                console.log('Mission succesfully marked as done!');
+                menu();
+            }
+        });
+    }
+}
