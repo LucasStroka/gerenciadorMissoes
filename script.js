9c8fe6fb8c552b44b99499aba561079e65441f4f
vitor-2006 <vitorbalbinotdellatorre@gmail.com>
2025-07-24 09:31:24 -0300

    Ranking de Destinos

--- script.js
+++ script.js
@@ -19,5 +19,26 @@ let mission = {
 
 const arrayMission = []
 
+RankingDestinos()
+function RankingDestinos(){
+    if (arrayMission === 0){
+        console.log("Nenhuma missão em andamento. \nPrecione ENTER para voltar...")
+        rl.question('', MenuPrincipal)
+    }else{
+        rl.question('Qual destino você quer o ranking?: ', (input) => {
+        let certo = input.toLowerCase().trim()
+        let ranking = 0
+            for(i = 0; i < arrayMission.length; i++){
+                if(arrayMission[i].includes(certo)){
+                    ranking++
+                }
+            }
+            console.log(`Missão: ${certo} \nRanking: ${ranking} missões`)
+            console.log("\nPrecione ENTER para voltar...")
+            rl.question('', MenuPrincipal)
+        })
+    }
+}
+
 
 
